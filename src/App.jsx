8ef2ad3449eba01b4d283dd571ca68082d528ebf
Krisko83 <kriss_eidg@yahoo.com>
2026-09-22 14:45:39 +0300

    Extract components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-function App() {
+import Footer from "./components/Footer.jsx";
+import Header from "./components/Header.jsx";
+import Pagination from "./components/Pagination.jsx";
+import UserList from "./components/UserList.jsx";
+import './styles.css'
 
+function App() {
 
   return (
     <>
-      <h1>Hello there!</h1>
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <UserList />
+
+          <button className="btn-add btn">Add new user</button>
+
+          <Pagination />
+        </section>
+      </main >
+      <Footer />
     </>
   )
 }
