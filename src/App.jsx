3f732ebde6e43bb44c5e47b3633fb62384a1a4fb
Krisko83 <kriss_eidg@yahoo.com>
2026-09-22 15:34:45 +0300

    Showing users in table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import Pagination from "./components/Pagination.jsx";
 import UserList from "./components/UserList.jsx";
 import './styles.css'
+import UserSearch from "./components/UserSearch.jsx";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://jekwxfohagnknpkqdgdo.supabase.co/rest/v1/users', {
+      headers: {
+        apikey: "sb_publishable_5H15oY-8n1QtXELqWorURg_FvFk-0Ha"
+      }
+    })
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.log('Fetching error: ', err)
+      )
+  }, []);
+
 
   return (
     <>
@@ -12,13 +29,17 @@ function App() {
 
       <main className="main">
         <section className="card users-container">
-          <UserList />
+
+          <UserSearch />
+
+          <UserList users={users} />
 
           <button className="btn-add btn">Add new user</button>
 
           <Pagination />
         </section>
       </main >
+
       <Footer />
     </>
   )
